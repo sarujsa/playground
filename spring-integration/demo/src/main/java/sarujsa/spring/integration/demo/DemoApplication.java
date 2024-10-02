747739b7704ad8470add62823eefe44670015dc8
sarujsa <sarujs@protonmail.com>
2024-10-02 23:06:03 +0200

    in file -> out file

--- spring-integration/demo/src/main/java/sarujsa/spring/integration/demo/DemoApplication.java
+++ spring-integration/demo/src/main/java/sarujsa/spring/integration/demo/DemoApplication.java
@@ -8,15 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.filters.AcceptOnceFileListFilter;
 import org.springframework.integration.file.filters.CompositeFileListFilter;
 import org.springframework.integration.file.filters.SimplePatternFileListFilter;
-import org.springframework.integration.file.transformer.FileToStringTransformer;
-import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.messaging.MessageChannel;
 
 import java.io.File;
@@ -26,6 +24,9 @@ public class DemoApplication {
 
   @Value("${in.file.dir.path}")
   private String inDirectoryName;
+
+  @Value("${out.file.dir.path}")
+  private String outDirectoryName;
 
   public static void main(String[] args) {
     var context =
@@ -37,34 +38,26 @@ public class DemoApplication {
     return new DirectChannel();
   }
 
-  @Bean MessageChannel stringifiedFileInputChannel() {
-    return new DirectChannel();
-  }
-
   @Bean
   @InboundChannelAdapter(channel = "fileInputChannel", poller = @Poller(fixedDelay = "1000"))
   public MessageSource<File> fileInbound() {
     FileReadingMessageSource source = new FileReadingMessageSource();
-    CompositeFileListFilter<File> compositeFileListFilter = new CompositeFileListFilter();
+    CompositeFileListFilter<File> compositeFileListFilter = new CompositeFileListFilter<>();
     compositeFileListFilter.addFilter(new SimplePatternFileListFilter("*.txt"));
     compositeFileListFilter.addFilter(new AcceptOnceFileListFilter<>());
+    source.setFilter(compositeFileListFilter);
     source.setDirectory(new File(inDirectoryName));
     return source;
   }
 
   @Bean
-  @ServiceActivator(inputChannel = "stringifiedFileInputChannel")
-  public LoggingHandler logging() {
-    LoggingHandler handler = new LoggingHandler(LoggingHandler.Level.INFO);
-    handler.setLoggerName("test_logger");
-    handler.setLogExpressionString("headers.id + ': ' + payload");
+  @ServiceActivator(inputChannel = "fileInputChannel")
+  public FileWritingMessageHandler fileOutbound() {
+    FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(outDirectoryName));
+    handler.setExpectReply(false);
+    handler.setDeleteSourceFiles(true);
+    handler.setAutoCreateDirectory(true);
     return handler;
-  }
-
-  @Bean
-  @Transformer(inputChannel = "fileInputChannel", outputChannel = "stringifiedFileInputChannel")
-  public FileToStringTransformer fileToStringTransformer() {
-    return new FileToStringTransformer();
   }
 
 }
