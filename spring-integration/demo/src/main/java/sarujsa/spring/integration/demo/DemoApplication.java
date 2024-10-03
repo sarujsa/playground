fa5b5972150e92db557bf6f71d239cc8c0142b6f
sarujsa <sarujs@protonmail.com>
2024-10-03 22:40:00 +0200

    added logging for each file

--- spring-integration/demo/src/main/java/sarujsa/spring/integration/demo/DemoApplication.java
+++ spring-integration/demo/src/main/java/sarujsa/spring/integration/demo/DemoApplication.java
@@ -1,21 +1,26 @@
 package sarujsa.spring.integration.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.interceptor.WireTap;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.filters.AcceptOnceFileListFilter;
 import org.springframework.integration.file.filters.CompositeFileListFilter;
 import org.springframework.integration.file.filters.SimplePatternFileListFilter;
+import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
 
 import java.io.File;
 
@@ -28,6 +33,9 @@ public class DemoApplication {
   @Value("${out.file.dir.path}")
   private String outDirectoryName;
 
+  @Autowired
+  private ApplicationContext context;
+
   public static void main(String[] args) {
     var context =
         new SpringApplicationBuilder(DemoApplication.class).web(WebApplicationType.NONE).run();
@@ -35,7 +43,28 @@ public class DemoApplication {
 
   @Bean
   public MessageChannel fileInputChannel() {
+    DirectChannel channel = new DirectChannel();
+    // Java DSL solution for wiretap: (instead of explicit bean creation and fetching)
+    //MessageChannels.direct().wireTap("logChannel");
+    channel.addInterceptor((ChannelInterceptor) context.getBean("wireTap"));
+    return channel;
+  }
+
+  @Bean
+  public WireTap wireTap() {
+    return new WireTap("logChannel");
+  }
+
+  @Bean
+  public MessageChannel logChannel() {
     return new DirectChannel();
+  }
+
+  @Bean
+  @ServiceActivator(inputChannel = "logChannel")
+  public LoggingHandler loggingHandler() {
+    LoggingHandler handler = new LoggingHandler(LoggingHandler.Level.INFO);
+    return handler;
   }
 
   @Bean
